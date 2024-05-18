c211777597169edaaa8757404895a21daaa7ad0a
Jesper Heuver <jesper@heuver.net>
2024-05-18 23:07:04 +0200

    add a check on whether we run this as standalone or import as module

--- comm_test/passData.py
+++ comm_test/passData.py
@@ -10,10 +10,11 @@ def write_read(x : str):
 	data = arduino.readline() 
 	return data 
 
-time.sleep(5) # wait until device is setup
-while True: 
-	num = input("Enter a number: ") # Taking input from user 
-	value = write_read(num) 
-	print(value.decode().strip()) # printing the value (decode bytes first, strip off the ctrl+lf println returns)
+if __name__ == "__main__": # enables us to import the function but not run the main code, as well as running the main code when we want to test. see https://stackoverflow.com/questions/419163/what-does-if-name-main-do
+	time.sleep(5) # wait until device is setup
+	while True: 
+		num = input("Enter a number: ") # Taking input from user 
+		value = write_read(num) 
+		print(value.decode().strip()) # printing the value (decode bytes first, strip off the ctrl+lf println returns)
 
 
