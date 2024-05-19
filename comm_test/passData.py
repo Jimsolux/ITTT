d759a4e01d5cec492bfb729170a1e3f0c8369c6a
Jesper Heuver <jesper@heuver.net>
2024-05-19 15:53:13 +0200

    make object for arduino comms

--- comm_test/passData.py
+++ comm_test/passData.py
@@ -2,19 +2,22 @@
 import serial 
 import time 
 
-arduino = serial.Serial(port='COM6', baudrate=9600, timeout=1) 
+class ArduinoComm:
+	def __init__(self):
+		self.arduinoSerial = serial.Serial('com6', 9600, timeout=1) #opens comport
 
-def write_read(x : str): 
-	bytestr = bytes(x, 'utf-8')
-	arduino.write(bytestr) 
-	data = arduino.readline() 
-	return data 
+	def write_read (self, x : str): 
+		bytestr = bytes(x, 'utf-8')
+		self.arduinoSerial.write(bytestr) 
+		data = self.arduinoSerial.readline() 
+		return data.decode().strip()
 
 if __name__ == "__main__": # enables us to import the function but not run the main code, as well as running the main code when we want to test. see https://stackoverflow.com/questions/419163/what-does-if-name-main-do
-	time.sleep(5) # wait until device is setup
+	time.sleep(5) # wait until device is setup 
+	arduino = ArduinoComm()
 	while True: 
 		num = input("Enter a number: ") # Taking input from user 
-		value = write_read(num) 
-		print(value.decode().strip()) # printing the value (decode bytes first, strip off the ctrl+lf println returns)
+		value = arduino.write_read(num) 
+		print(value) # printing the value (decode bytes first, strip off the ctrl+lf println returns)
 
 
